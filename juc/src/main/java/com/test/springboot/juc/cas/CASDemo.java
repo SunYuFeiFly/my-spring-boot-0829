b9a8cfca2d18b7807ec2a2b93738d060f7d30872
gaozy124 <298012895@qq.com>
2022-07-14 15:06:31 +0800

    "AtomicInteger原子操作类"

--- juc/src/main/java/com/test/springboot/juc/cas/CASDemo.java
+++ juc/src/main/java/com/test/springboot/juc/cas/CASDemo.java
@@ -24,7 +24,22 @@ public class CASDemo {
         AtomicInteger atomicInteger = new AtomicInteger(10);
         // 第一次对比修改前值符合，数值已经修改为2022
         System.out.println(atomicInteger.compareAndSet(10, 2022) + " " + atomicInteger.get());
+
+        // 相当于i++
+        System.out.println(atomicInteger.getAndIncrement());
         // 第二次对比数值不等，数值未修改
-        System.out.println(atomicInteger.compareAndSet(10, 2022) + " " + atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(2023, 100) + " " + atomicInteger.get());
+
+        // 相当于i--
+        System.out.println(atomicInteger.getAndDecrement());
+        System.out.println(atomicInteger.compareAndSet(99, 12138) + " " + atomicInteger.get());
+
+        // 相当于++i
+        System.out.println(atomicInteger.incrementAndGet());
+        System.out.println(atomicInteger.compareAndSet(12139, 55) + " " + atomicInteger.get());
+
+        // 相当于--i
+        System.out.println(atomicInteger.decrementAndGet());
+        System.out.println(atomicInteger.compareAndSet(54, 99) + " " + atomicInteger.get());
     }
 }
