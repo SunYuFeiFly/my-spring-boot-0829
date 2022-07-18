a75839f297ad530895b96dd4f93eb84676dd51e8
gaozy124 <298012895@qq.com>
2022-07-18 17:17:21 +0800

    "卖票总数（加锁原始统计方法）、个人卖票数量统计（ThreadLocal）"

--- juc/src/main/java/com/test/springboot/juc/threadLocal/ThreadLocalDemo.java
+++ juc/src/main/java/com/test/springboot/juc/threadLocal/ThreadLocalDemo.java
@@ -1,18 +1,47 @@
 package com.test.springboot.juc.threadLocal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author syf_12138
- * @Description 卖票总数、个人卖票数量统计
+ * @Description 卖票总数（加锁原始统计方法）、个人卖票数量统计（ThreadLocal）
  * @create 2022/7/18 16:33
  */
 
 public class ThreadLocalDemo {
 
     public static void main(String[] args) {
-        testSynchronized();
+        // testSynchronized();
+        House house = new House();
+        for (int i = 1; i <= 5; i++) {
+            new Thread(() -> {
+                int size = new Random().nextInt(5) + 1;
+                try {
+                    for (int j = 0; j < size; j++) {
+                        // 用于统计所有销售总数
+                        house.saleHouse();
+                        // 用于计算单个销售数量
+                        house.saleVolumeByThreadLocal();
+                    }
+                    // 获取每一个线程实际操作的数额
+                    System.out.println(Thread.currentThread().getName() + "销售房屋数量为： " + house.saleVolume.get());
+                } finally {
+                    house.saleVolume.remove();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+        // 暂停一定时间让线程执行完
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("销售房屋总数为： " + house.saleCount);
     }
 
     /**
@@ -52,5 +81,20 @@ class House {
 
     public synchronized void saleHouse() {
         ++saleCount;
+    }
+
+    // 老版创建ThreadLocal对象方法
+//    ThreadLocal<Integer> threadLocal = new ThreadLocal<Integer>(){
+//        @Override
+//        protected Integer initialValue() {
+//            return 0;
+//        }
+//    };
+
+     ThreadLocal<Integer> saleVolume = ThreadLocal.withInitial(() -> 0);
+
+    // 利用ThreadLocal真正进行数据统计
+    public void saleVolumeByThreadLocal() {
+        saleVolume.set(1 + saleVolume.get());
     }
 }
