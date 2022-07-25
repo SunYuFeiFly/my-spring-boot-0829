7a04ed2b55e9040a6d3a69af44d8b25f33023ef5
gaozy124 <298012895@qq.com>
2022-07-25 15:54:10 +0800

    "自定义MD5加密realm测试"

---
+++ springboot_shiro_baizhi/springboot-shiro-config/src/main/java/com/baizhi/shiro/test/TestMD5RealmShiro.java
@@ -0,0 +1,43 @@
+package com.baizhi.shiro.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+
+/**
+ * @author syf_12138
+ * @Description 自定义MD5加密realm测试
+ * @create 2022/7/25 11:58
+ */
+
+public class TestMD5RealmShiro {
+
+    public static void main(String[] args) {
+        // 创建安全管理器工厂
+        IniSecurityManagerFactory securityManagerFactory = new IniSecurityManagerFactory("classpath:shiro-md5-realm.ini");
+        // 通过工厂获取安全管理器
+        SecurityManager securityManager = securityManagerFactory.getInstance();
+        // 将安全管理器注入到Security工具类中
+        SecurityUtils.setSecurityManager(securityManager);
+        // 获取主体对象
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan", "123456");
+        System.out.println("认证前状态：" + subject.isAuthenticated());
+        try {
+            // 登录操作
+            subject.login(token);
+        } catch (UnknownAccountException e) {
+            System.out.println("用户名不存在");
+        } catch (IncorrectCredentialsException e) {
+            System.out.println("用户密码错误");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("认证后状态：" + subject.isAuthenticated());
+    }
+}
