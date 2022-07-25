50125572a8c96fdffc03c2f4c4829eac68b64f4e
gaozy124 <298012895@qq.com>
2022-07-25 10:45:43 +0800

    "springboot整合shiro案列代码初次上传"

---
+++ springboot_shiro_baizhi/springboot-shiro-config/src/main/java/com/baizhi/shiro/SpringbootShiroConfigApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroConfigApplication.class, args);
+    }
+
+}
---
+++ springboot_shiro_baizhi/springboot-shiro-config/src/test/java/com/baizhi/shiro/SpringbootShiroConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShiroConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
