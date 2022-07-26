cab5244a339c586a0d7d52ebb20c9077f0c59136
gaozy124 <298012895@qq.com>
2022-07-26 11:25:52 +0800

    "springboot连接数据库整合shiro初次上传代码"

---
+++ springboot_shiro_baizhi/springboot-shiro-mysql/src/main/java/com/baizhi/shiro/SpringbootShiroMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroMysqlApplication.class, args);
+    }
+
+}
---
+++ springboot_shiro_baizhi/springboot-shiro-mysql/src/main/java/com/baizhi/shiro/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.baizhi.shiro.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author syf_12138
+ * @Description 测试构建项目基本功能
+ * @create 2022/7/26 11:18
+ */
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("hello")
+    public String hello() {
+        return "springboot shiro hello !!!";
+    }
+}
