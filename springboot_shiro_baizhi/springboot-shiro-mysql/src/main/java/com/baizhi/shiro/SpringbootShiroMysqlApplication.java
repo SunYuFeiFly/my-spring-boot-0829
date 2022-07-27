6a8a6f7c36f91aeaaad32d67dda7acb6f6622bd2
gaozy124 <298012895@qq.com>
2022-07-27 13:48:04 +0800

    "映射包路径相应修改"

--- springboot_shiro_baizhi/springboot-shiro-mysql/src/main/java/com/baizhi/shiro/SpringbootShiroMysqlApplication.java
+++ springboot_shiro_baizhi/springboot-shiro-mysql/src/main/java/com/baizhi/shiro/SpringbootShiroMysqlApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages="com.baizhi.shiro.dao")
+@MapperScan(basePackages="com.baizhi.shiro.mapper")
 @SpringBootApplication
 public class SpringbootShiroMysqlApplication {
 
