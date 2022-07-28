ed84c3ea3e2606c23748c79a33c7557b85019f11
gaozy124 <298012895@qq.com>
2022-07-28 18:00:21 +0800

    "引用对象实体类"

---
+++ spring-boot-01-atguigu/spring-boot-02-config/src/main/java/com/atguigu/springboot/bean/Dog.java
@@ -0,0 +1,45 @@
+package com.atguigu.springboot.bean;
+
+/**
+ * @author syf_12138
+ * @Description 测试配置文件值注入
+ * @create 2022/7/28 16:09
+ */
+
+public class Dog {
+
+    private String name;
+    private int age;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
