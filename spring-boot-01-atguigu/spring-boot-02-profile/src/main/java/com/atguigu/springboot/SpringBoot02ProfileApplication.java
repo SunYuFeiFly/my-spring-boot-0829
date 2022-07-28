e3a7d629f5bbee543df0a7206c88814a199a535b
syf_12138 <1049924914@qq.com>
2022-07-28 22:58:51 +0800

    "设置启动端口的几种方式"

--- spring-boot-01-atguigu/spring-boot-02-profile/src/main/java/com/atguigu/springboot/SpringBoot02ProfileApplication.java
+++ spring-boot-01-atguigu/spring-boot-02-profile/src/main/java/com/atguigu/springboot/SpringBoot02ProfileApplication.java
@@ -3,6 +3,16 @@ package com.atguigu.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author syf_12138
+ * @Description 指定启动端口的方式
+ *      1、yml代码块配置
+ *      2、properties多文件指定启动文件
+ *      3、vim设置启动端口
+ *      4、运行打包好的jar是设置server.port值
+ * @create 2022/7/28 22:55
+ */
+
 @SpringBootApplication
 public class SpringBoot02ProfileApplication {
 
