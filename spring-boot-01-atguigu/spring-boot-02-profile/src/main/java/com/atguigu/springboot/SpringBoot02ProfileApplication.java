557ec3085f7f1aa3b13fff02782cdb90b6a123ab
syf_12138 <1049924914@qq.com>
2022-07-28 22:46:54 +0800

    "spring boot 多环境配置"

---
+++ spring-boot-01-atguigu/spring-boot-02-profile/src/main/java/com/atguigu/springboot/SpringBoot02ProfileApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02ProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02ProfileApplication.class, args);
+    }
+
+}
