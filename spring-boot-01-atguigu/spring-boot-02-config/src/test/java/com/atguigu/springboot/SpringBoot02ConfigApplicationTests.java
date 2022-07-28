9455c3b74520d509ca995a7a6be130b59a715d4e
gaozy124 <298012895@qq.com>
2022-07-28 17:55:39 +0800

    "Spring Boot 测试值注入效果"

--- spring-boot-01-atguigu/spring-boot-02-config/src/test/java/com/atguigu/springboot/SpringBoot02ConfigApplicationTests.java
+++ spring-boot-01-atguigu/spring-boot-02-config/src/test/java/com/atguigu/springboot/SpringBoot02ConfigApplicationTests.java
@@ -1,8 +1,34 @@
 package com.atguigu.springboot;
 
+import com.atguigu.springboot.bean.Person01;
+import com.atguigu.springboot.bean.Person02;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBoot02ConfigApplicationTests {
+public class SpringBoot02ConfigApplicationTests {
+
+    @Autowired
+    Person01 person;
+
+    @Autowired
+    Person02 person02;
+
+    @Test
+    public void contextLoads() {
+        System.out.println("person：" + person);
+        for (String list : person.getLists()) {
+            System.out.println("iter: " + list);
+        }
+    }
+
+    @Test
+    public void getPerson02() {
+        System.out.println("person02:" + person02);
+    }
 
 }
