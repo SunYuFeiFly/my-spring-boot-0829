a360e9837e6067d2dc9591a767b1bc2dd379106e
syf_12138 <1049924914@qq.com>
2022-07-28 22:23:44 +0800

    "用于配合测试ImportResource 导入配置文件测试实体类"

--- spring-boot-01-atguigu/spring-boot-02-config/src/test/java/com/atguigu/springboot/SpringBoot02ConfigApplicationTests.java
+++ spring-boot-01-atguigu/spring-boot-02-config/src/test/java/com/atguigu/springboot/SpringBoot02ConfigApplicationTests.java
@@ -2,10 +2,12 @@ package com.atguigu.springboot;
 
 import com.atguigu.springboot.bean.Person01;
 import com.atguigu.springboot.bean.Person02;
+import com.atguigu.springboot.bean.Person03;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -18,6 +20,12 @@ public class SpringBoot02ConfigApplicationTests {
     @Autowired
     Person02 person02;
 
+    @Autowired
+    Person03 person03;
+
+    @Autowired
+    ApplicationContext ioc;
+
     @Test
     public void contextLoads() {
         System.out.println("person：" + person);
@@ -29,6 +37,22 @@ public class SpringBoot02ConfigApplicationTests {
     @Test
     public void getPerson02() {
         System.out.println("person02:" + person02);
+    }
+
+
+    @Test
+    public void getPerson03() {
+        System.out.println("person03:" + person03);
+    }
+
+
+    /**
+     * 用于测试ImportResource 导入配置文件功能
+     */
+    @Test
+    public void testHelloService(){
+        boolean b = ioc.containsBean("helloService02");
+        System.out.println(b);
     }
 
 }
