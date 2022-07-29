172507678efaaf1e581e535d0f5ec177338df725
gaozy124 <298012895@qq.com>
2022-07-29 17:17:11 +0800

    "自动化配置"

--- spring-boot-01-atguigu/spring-boot-02-config-autoconfig/src/main/java/com/atguigu/springboot/SpringBoot02ConfigAutoconfigApplication.java
+++ spring-boot-01-atguigu/spring-boot-02-config-autoconfig/src/main/java/com/atguigu/springboot/SpringBoot02ConfigAutoconfigApplication.java
@@ -3,6 +3,10 @@ package com.atguigu.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类，相关自动化配置原理看附带文档
+ */
+
 @SpringBootApplication
 public class SpringBoot02ConfigAutoconfigApplication {
 
