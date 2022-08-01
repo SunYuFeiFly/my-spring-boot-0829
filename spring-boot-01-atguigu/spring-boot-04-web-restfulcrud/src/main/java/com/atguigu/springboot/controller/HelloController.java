97a6098099e5870c155e9a2909d1664c14d40515
gaozy124 <298012895@qq.com>
2022-08-01 17:53:45 +0800

    "测试thymeleaf功能"

--- spring-boot-01-atguigu/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ spring-boot-01-atguigu/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -4,18 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * @author syf_12138
  * @Description 简单controller功能测试
- *              1、所有 /webjars/** ，都去 classpath:/META-INF/resources/webjars/ 找资源；localhost:8080/webjars/jquery/3.3.1/jquery.js
- *              2、"/**" 访问当前项目的任何资源，都去（静态资源的文件夹）找映射
- *                  "classpath:/META‐INF/resources/"
- *                  "classpath:/resources/"
- *                  "classpath:/static/"
- *                  "classpath:/public/"
- *                  "/"：当前项目的根路径
- *               3、欢迎页； 静态资源文件夹下的所有index.html页面；被"/**"映射；
- *               4、所有的 /favicon.ico 都是在静态资源文件下找（图标）
+ * 1、所有 /webjars/** ，都去 classpath:/META-INF/resources/webjars/ 找资源；localhost:8080/webjars/jquery/3.3.1/jquery.js
+ * 2、"/**" 访问当前项目的任何资源，都去（静态资源的文件夹）找映射
+ * "classpath:/META‐INF/resources/"
+ * "classpath:/resources/"
+ * "classpath:/static/"
+ * "classpath:/public/"
+ * "/"：当前项目的根路径
+ * 3、欢迎页； 静态资源文件夹下的所有index.html页面；被"/**"映射；
+ * 4、所有的 /favicon.ico 都是在静态资源文件下找（图标）
  * @create 2022/8/1 16:00
  */
 
@@ -27,5 +30,14 @@ public class HelloController {
     public String hello() {
         System.out.println("hello springboot!!!");
         return "hello springboot!!!";
+    }
+
+    @RequestMapping("testThymeleaf")
+    public String testThymeleaf(Map<String, Object> map) {
+        System.out.println("测试thymeleaf!!!");
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
+        // 会跳转到/templates/success.html文件
+        return "success";
     }
 }
