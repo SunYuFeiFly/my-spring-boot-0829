f987502213baca9298b5fe23130b9e5c5bccebb2
gaozy124 <298012895@qq.com>
2022-08-01 16:53:33 +0800

    "外部静态资源、内部静态资源、欢迎页、图标等访问路径说明"

--- spring-boot-01-atguigu/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ spring-boot-01-atguigu/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -7,6 +7,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * @author syf_12138
  * @Description 简单controller功能测试
+ *              1、所有 /webjars/** ，都去 classpath:/META-INF/resources/webjars/ 找资源；localhost:8080/webjars/jquery/3.3.1/jquery.js
+ *              2、"/**" 访问当前项目的任何资源，都去（静态资源的文件夹）找映射
+ *                  "classpath:/META‐INF/resources/"
+ *                  "classpath:/resources/"
+ *                  "classpath:/static/"
+ *                  "classpath:/public/"
+ *                  "/"：当前项目的根路径
+ *               3、欢迎页； 静态资源文件夹下的所有index.html页面；被"/**"映射；
+ *               4、所有的 /favicon.ico 都是在静态资源文件下找（图标）
  * @create 2022/8/1 16:00
  */
 
