0a0de3c203038e926f1b674105d7667998ae1ee3
gaozy124 <298012895@qq.com>
2022-08-01 17:55:28 +0800

    "Spring Boot 测试文件"

--- spring-boot-01-atguigu/spring-boot-04-web-restfulcrud/src/test/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplicationTests.java
+++ spring-boot-01-atguigu/spring-boot-04-web-restfulcrud/src/test/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplicationTests.java
@@ -1,10 +1,13 @@
 package com.atguigu.springboot;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBoot04WebRestfulcrudApplicationTests {
+public class SpringBoot04WebRestfulcrudApplicationTests {
 
     @Test
     void contextLoads() {
