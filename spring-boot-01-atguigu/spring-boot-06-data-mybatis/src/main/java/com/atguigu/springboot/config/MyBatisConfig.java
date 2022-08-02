0b3301406b80c5aac5fb129e77f8c5c117ef0825
gaozy124 <298012895@qq.com>
2022-08-02 15:33:23 +0800

    "mybatis配置（驼峰命名）"

---
+++ spring-boot-01-atguigu/spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/config/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.springboot.config;
+
+import org.apache.ibatis.session.Configuration;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author syf_12138
+ * @Description mybatis配置（驼峰命名）
+ * @create 2022/8/2 15:32
+ */
+
+@org.springframework.context.annotation.Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer(){
+        return new ConfigurationCustomizer(){
+            @Override
+            public void customize(Configuration configuration) {
+                configuration.setMapUnderscoreToCamelCase(true);
+            }
+        };
+    }
+}
