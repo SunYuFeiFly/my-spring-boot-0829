59ae00922a99c87071bda1c96ba2f32e888a161d
gaozy124 <298012895@qq.com>
2022-08-02 16:36:50 +0800

    "Spring Boot 启动配置原理"

---
+++ spring-boot-01-atguigu/spring-boot-07-theory/src/main/java/com/atguigu/springboot/SpringBoot07TheoryApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07TheoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot07TheoryApplication.class, args);
+    }
+
+}
