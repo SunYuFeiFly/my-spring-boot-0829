9a3b10e95a8f95fa0994ebafd700b9778fb785e2
gaozy124 <298012895@qq.com>
2022-08-02 11:03:13 +0800

    "自定义区域地区解析器"

---
+++ spring-boot-01-atguigu/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/component/MyLocaleResolver.java
@@ -0,0 +1,34 @@
+package com.atguigu.springboot.component;
+
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * @author syf_12138
+ * @Description 区域地区解析器
+ * @create 2022/8/2 11:00
+ */
+
+public class MyLocaleResolver implements LocaleResolver {
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String l = request.getParameter("l");
+        Locale locale = Locale.getDefault();
+        if(!StringUtils.isEmpty(l)){
+            String[] split = l.split("_");
+            locale = new Locale(split[0],split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+
+    }
+}
