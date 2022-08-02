72955aac8acbc3de65ec5aacd81cfd0d819247b5
gaozy124 <298012895@qq.com>
2022-08-02 13:44:29 +0800

    "Spring Boot 简单数据配置获取数据源验证"

--- spring-boot-01-atguigu/spring-boot-06-data-jdbc/src/test/java/com/atguigu/springboot/SpringBoot06DataJdbcApplicationTests.java
+++ spring-boot-01-atguigu/spring-boot-06-data-jdbc/src/test/java/com/atguigu/springboot/SpringBoot06DataJdbcApplicationTests.java
@@ -1,13 +1,23 @@
 package com.atguigu.springboot;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBoot06DataJdbcApplicationTests {
+public class SpringBoot06DataJdbcApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println("dataSource: " + dataSource);
     }
 
 }
