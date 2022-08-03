4c1b129322638a72a6461125d6fc1872ae0d414f
gaozy124 <298012895@qq.com>
2022-08-03 17:41:59 +0800

    "Spring Boot 整合elastic"

---
+++ spring-boot-01-atguigu/springboot-03-elastic/src/test/java/com/atguigu/elastic/Springboot03ElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03ElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
