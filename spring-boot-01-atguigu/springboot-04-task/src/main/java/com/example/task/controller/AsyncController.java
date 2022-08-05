ba129ffaeb2a19079033824523646d034b73aa9c
gaozy124 <298012895@qq.com>
2022-08-05 15:15:11 +0800

    "Spring Boot 异步任务"

--- spring-boot-01-atguigu/springboot-04-task/src/main/java/com/example/task/controller/AsyncController.java
+++ spring-boot-01-atguigu/springboot-04-task/src/main/java/com/example/task/controller/AsyncController.java
@@ -2,8 +2,8 @@ package com.example.task.controller;
 
 import com.example.task.service.AsyncService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author syf_12138
@@ -11,12 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @create 2022/8/4 18:02
  */
 
-@Controller
+@RestController
 public class AsyncController {
 
     @Autowired
     private AsyncService asyncService;
 
+    /**
+     * asyncService调用的hello()方法是异步方法，所以当运行到hello方法后，并没有因此等hello()执行完再作打印任务，而是直接让hello()新建线程执行，同时打印内容。
+     * @return
+     */
     @GetMapping("/testAsync")
     public String testAsync() {
         asyncService.hello();
