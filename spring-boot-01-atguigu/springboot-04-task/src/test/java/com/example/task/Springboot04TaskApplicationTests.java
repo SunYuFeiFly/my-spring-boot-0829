563dbbf5744ce448ee39d0ea0fab94581fd67919
gaozy124 <298012895@qq.com>
2022-08-05 15:50:46 +0800

    "Spring Boot 发送邮件测试"

--- spring-boot-01-atguigu/springboot-04-task/src/test/java/com/example/task/Springboot04TaskApplicationTests.java
+++ spring-boot-01-atguigu/springboot-04-task/src/test/java/com/example/task/Springboot04TaskApplicationTests.java
@@ -4,8 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,15 +24,37 @@ public class Springboot04TaskApplicationTests {
      * 发送邮件
      */
     @Test
-    void testSendMail() {
-
+    public void testSendMail() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        // 主题
+        message.setSubject("发简单邮箱测试");
+        // 具体内容
+        message.setText("邮箱测试");
+        message.setTo("1049924914@qq.com");
+        message.setFrom("2077384801@qq.com");
+        mailSender.send(message);
     }
 
     /**
      * 发送复杂邮件
      */
     @Test
-    void testSendComplexMail() {
+    public void testSendComplexMail() throws MessagingException {
+        //1、创建一个复杂的消息邮件
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
+        //邮件设置
+        helper.setSubject("通知-今晚开会");
+        helper.setText("<b style='color:red'>今天 7:30 开会</b>",true);
+
+        helper.setTo("17512080612@163.com");
+        helper.setFrom("534096094@qq.com");
+
+        //上传文件
+        helper.addAttachment("1.jpg",new File("C:\\Users\\lfy\\Pictures\\Saved Pictures\\1.jpg"));
+        helper.addAttachment("2.jpg",new File("C:\\Users\\lfy\\Pictures\\Saved Pictures\\2.jpg"));
+
+        mailSender.send(mimeMessage);
     }
 }
