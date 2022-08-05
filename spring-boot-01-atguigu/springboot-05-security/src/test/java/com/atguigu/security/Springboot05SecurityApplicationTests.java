f55e5ce104fcfaa0330d338140d7e7ed7e1236b0
gaozy124 <298012895@qq.com>
2022-08-05 18:01:49 +0800

    "测试类"

--- spring-boot-01-atguigu/springboot-05-security/src/test/java/com/atguigu/security/Springboot05SecurityApplicationTests.java
+++ spring-boot-01-atguigu/springboot-05-security/src/test/java/com/atguigu/security/Springboot05SecurityApplicationTests.java
@@ -1,13 +1,18 @@
 package com.atguigu.security;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class Springboot05SecurityApplicationTests {
+public class Springboot05SecurityApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
+
     }
 
 }
