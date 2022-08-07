134bb5c63ee0070ae554a8d76f4e753900a76f2a
syf_12138 <1049924914@qq.com>
2022-08-07 20:11:11 +0800

    "受保护资源控制类"

---
+++ springboot-security-baizhi/springboot-security-02-customize/src/main/java/com/baizi/security/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.baizi.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author syf_12138
+ * @Description 受保护资源控制类
+ * @Date 2022/8/7 19:27
+ **/
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello() {
+        System.out.println("hello security!!!");
+        return "hello security!!!";
+    }
+}
