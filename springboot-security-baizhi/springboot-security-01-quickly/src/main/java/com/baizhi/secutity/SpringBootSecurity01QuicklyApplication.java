d2634b0e465c56f8fc7624021e1d834dc696fb33
syf_12138 <1049924914@qq.com>
2022-08-07 15:06:42 +0800

    "springboot整合security简单创建项目测试"

---
+++ springboot-security-baizhi/springboot-security-01-quickly/src/main/java/com/baizhi/secutity/SpringBootSecurity01QuicklyApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.secutity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurity01QuicklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurity01QuicklyApplication.class, args);
+    }
+
+}
---
+++ springboot-security-baizhi/springboot-security-01-quickly/src/test/java/com/baizhi/secutity/SpringBootSecurity01QuicklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.secutity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurity01QuicklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
