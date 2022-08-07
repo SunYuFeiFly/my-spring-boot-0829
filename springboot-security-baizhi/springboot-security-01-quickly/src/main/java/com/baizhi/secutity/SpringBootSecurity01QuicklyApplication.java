066faf48cc33719852adcdd22047525ee37eb7bf
syf_12138 <1049924914@qq.com>
2022-08-07 15:23:08 +0800

    "security开启使用流程"

--- springboot-security-baizhi/springboot-security-01-quickly/src/main/java/com/baizhi/secutity/SpringBootSecurity01QuicklyApplication.java
+++ springboot-security-baizhi/springboot-security-01-quickly/src/main/java/com/baizhi/secutity/SpringBootSecurity01QuicklyApplication.java
@@ -3,6 +3,10 @@ package com.baizhi.secutity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 默认引入security依赖便已经开始对所有请求起到拦截保护作用，且不用配置类进行配置，开启权限
+ */
+
 @SpringBootApplication
 public class SpringBootSecurity01QuicklyApplication {
 
