7066ecb951b52bfb038512c5b1039a50b1bc41da
syf_12138 <1049924914@qq.com>
2022-08-07 15:17:55 +0800

    "简单控制类"

---
+++ springboot-security-baizhi/springboot-security-01-quickly/src/main/java/com/baizhi/secutity/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.baizhi.secutity.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author syf_12138
+ * @Description 简单控制类
+ * @Date 2022/8/7 15:12
+ **/
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println("Hello Spring Security");
+        return "hello spring security";
+    }
+}
