03f0cda781b11bed517541c54688889077088d57
syf_12138 <1049924914@qq.com>
2022-08-07 15:09:25 +0800

    "简单测试类"

--- springboot-security-baizhi/springboot-security-01-quickly/src/test/java/com/baizhi/secutity/SpringBootSecurity01QuicklyApplicationTests.java
+++ springboot-security-baizhi/springboot-security-01-quickly/src/test/java/com/baizhi/secutity/SpringBootSecurity01QuicklyApplicationTests.java
@@ -1,13 +1,16 @@
 package com.baizhi.secutity;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBootSecurity01QuicklyApplicationTests {
+public class SpringBootSecurity01QuicklyApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
