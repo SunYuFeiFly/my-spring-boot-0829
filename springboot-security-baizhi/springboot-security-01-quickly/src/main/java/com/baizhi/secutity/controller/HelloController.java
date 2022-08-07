ec2e21a6ad2f8ec0ebd3a86e481b0b53348482d1
syf_12138 <1049924914@qq.com>
2022-08-07 15:19:56 +0800

    "简单控制类"

--- springboot-security-baizhi/springboot-security-01-quickly/src/main/java/com/baizhi/secutity/controller/HelloController.java
+++ springboot-security-baizhi/springboot-security-01-quickly/src/main/java/com/baizhi/secutity/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author syf_12138
- * @Description 简单控制类
+ * @Description 简单控制类（此时没有引入security，可以随意访问）
  * @Date 2022/8/7 15:12
  **/
 
