8645c7896fdc709590a9f29f48f4093a74956b43
syf_12138 <1049924914@qq.com>
2022-08-07 17:59:32 +0800

    "Spring Boot 自定义认证资源"

---
+++ springboot-security-baizhi/springboot-security-02-customize/src/main/java/com/baizi/security/SpringbootSecurity02CustomizeApplication.java
@@ -0,0 +1,13 @@
+package com.baizi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity02CustomizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity02CustomizeApplication.class, args);
+    }
+
+}
