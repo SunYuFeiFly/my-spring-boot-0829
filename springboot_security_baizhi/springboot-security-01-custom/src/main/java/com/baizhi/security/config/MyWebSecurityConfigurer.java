41ab15e0e7a4e8bd8750f9050b4c94bea4254212
gaozy124 <298012895@qq.com>
2022-08-09 15:12:30 +0800

    "自定义资源权限规则(此方法已过时)"

---
+++ springboot_security_baizhi/springboot-security-01-custom/src/main/java/com/baizhi/security/config/MyWebSecurityConfigurer.java
@@ -0,0 +1,37 @@
+package com.baizhi.security.config;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author syf_12138
+ * @Description TODO
+ * @create 2022/8/9 14:17
+ */
+
+/**
+ * @Author syf_12138
+ * @Description 自定义资源权限规则(此方法已过时)
+ * @Date 2022/8/7 19:55
+ **/
+
+// @Configuration
+public class MyWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 开启前请求权限管理
+        http.authorizeHttpRequests()
+                // 匹配请求
+                // 对于/index放行，无需认证和授权即可访问（方信息港资源写在anyRequest()之前）
+                .mvcMatchers("/index").permitAll()
+                // 所有请求在认证之后才能访问
+                .anyRequest().authenticated()
+                .and()
+                // 开启表单认证
+                .formLogin()
+                // 设置自己的登录页面
+                .loginPage("/login");
+    }
+}
