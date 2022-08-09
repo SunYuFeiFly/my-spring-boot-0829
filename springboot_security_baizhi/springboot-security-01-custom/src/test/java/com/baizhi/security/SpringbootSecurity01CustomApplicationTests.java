46edac77b713b825966f28b43fa9840fa1022ce0
gaozy124 <298012895@qq.com>
2022-08-09 12:41:52 +0800

    "Spring Boot 整合security，自定义认证授权"

---
+++ springboot_security_baizhi/springboot-security-01-custom/src/test/java/com/baizhi/security/SpringbootSecurity01CustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity01CustomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
