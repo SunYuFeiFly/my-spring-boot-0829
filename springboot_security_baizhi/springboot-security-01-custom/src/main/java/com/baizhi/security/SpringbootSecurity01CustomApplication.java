ff9c5cdcab6f3454a2affbce4df6953c7446517e
gaozy124 <298012895@qq.com>
2022-08-10 17:05:16 +0800

    "设置全局参数"

--- springboot_security_baizhi/springboot-security-01-custom/src/main/java/com/baizhi/security/SpringbootSecurity01CustomApplication.java
+++ springboot_security_baizhi/springboot-security-01-custom/src/main/java/com/baizhi/security/SpringbootSecurity01CustomApplication.java
@@ -2,12 +2,17 @@ package com.baizhi.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class SpringbootSecurity01CustomApplication {
 
     public static void main(String[] args) {
+        // System.setProperty("spring.security.strategy","MODE_INHERITABLETHREADLOCAL");
         SpringApplication.run(SpringbootSecurity01CustomApplication.class, args);
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
 }
