7e348053407268da75371a08c9825291792b62b3
gaozy124 <298012895@qq.com>
2022-08-11 12:00:03 +0800

    Spring Boot 整合Security Web项目

---
+++ springboot_security_baizhi/springboot-security-02-web/src/main/java/com/baizhi/security/SpringbootSecurity02WebApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity02WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity02WebApplication.class, args);
+    }
+
+}
---
+++ springboot_security_baizhi/springboot-security-02-web/src/test/java/com/baizhi/security/SpringbootSecurity02WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringbootSecurity02WebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
