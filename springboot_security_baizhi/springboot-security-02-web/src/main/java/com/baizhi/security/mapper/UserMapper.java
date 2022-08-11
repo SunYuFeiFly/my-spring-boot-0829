1ca5f615640afbbbf05b63f2efcd4b1b21d057e9
gaozy124 <298012895@qq.com>
2022-08-11 14:29:55 +0800

    用户映射接口

---
+++ springboot_security_baizhi/springboot-security-02-web/src/main/java/com/baizhi/security/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.baizhi.security.mapper;
+
+import com.baizhi.security.entity.Role;
+import com.baizhi.security.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author syf_12138
+ * @Description 用户映射接口
+ * @create 2022/8/11 14:18
+ */
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据用户名查找用户
+     */
+    User loadUserByUsername(@Param("username") String username);
+
+    /**
+     * 根据用户id查询一个角色，注意一个用户可能不止一种角色
+     */
+    List<Role> getRolesByUid(@Param("uid") Integer uid);
+}
