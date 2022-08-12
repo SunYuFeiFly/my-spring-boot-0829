39d333cf59fbfc6d1ba2c031598f4d150f8d547c
gaozy124 <298012895@qq.com>
2022-08-12 15:50:16 +0800

    自定义mvc配置

---
+++ springboot_security_baizhi/springboot-security-04-web-verifycode/src/main/java/com/baizhi/security/config/MyMvcConfigure.java
@@ -0,0 +1,21 @@
+package com.baizhi.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author syf_12138
+ * @Description 自定义mvc配置
+ * @create 2022/8/12 15:28
+ */
+
+@Configuration
+public class MyMvcConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+}
