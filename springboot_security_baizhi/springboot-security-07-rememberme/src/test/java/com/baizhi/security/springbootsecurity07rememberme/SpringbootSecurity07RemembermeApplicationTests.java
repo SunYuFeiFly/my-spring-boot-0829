5855732ef38f745804925e16c151dae95b96e13e
syf_12138 <1049924914@qq.com>
2022-08-14 23:07:31 +0800

    Spring Boot 整合Security 记住我功能

---
+++ springboot_security_baizhi/springboot-security-07-rememberme/src/test/java/com/baizhi/security/springbootsecurity07rememberme/SpringbootSecurity07RemembermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.security.springbootsecurity07rememberme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity07RemembermeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
