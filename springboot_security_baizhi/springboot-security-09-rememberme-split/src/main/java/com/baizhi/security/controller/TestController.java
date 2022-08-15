1250ea29c6db2fe36154ad41cd8372494234a892
gaozy124 <298012895@qq.com>
2022-08-15 16:19:29 +0800

    简单控制类测试

---
+++ springboot_security_baizhi/springboot-security-09-rememberme-split/src/main/java/com/baizhi/security/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.baizhi.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author syf_12138
+ * @Description 简单控制类测试
+ * @create 2022/8/15 15:17
+ */
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("test ....");
+        return "test ok!";
+    }
+}
