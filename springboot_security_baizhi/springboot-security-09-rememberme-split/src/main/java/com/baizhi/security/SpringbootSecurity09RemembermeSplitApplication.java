23ea31c485a1a5948cb87dad3f5b8d839197552c
gaozy124 <298012895@qq.com>
2022-08-15 15:16:11 +0800

    Spring Boot整合Security 完善记住我功能（前后端分离）

---
+++ springboot_security_baizhi/springboot-security-09-rememberme-split/src/main/java/com/baizhi/security/SpringbootSecurity09RemembermeSplitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity09RemembermeSplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity09RemembermeSplitApplication.class, args);
+    }
+
+}
