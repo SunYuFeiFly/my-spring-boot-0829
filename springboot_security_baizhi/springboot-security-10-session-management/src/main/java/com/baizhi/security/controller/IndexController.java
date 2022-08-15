986b646e82ef742c94b7a02574fcce2db8fbe5bc
gaozy124 <298012895@qq.com>
2022-08-15 16:47:56 +0800

    简单测试控制类

---
+++ springboot_security_baizhi/springboot-security-10-session-management/src/main/java/com/baizhi/security/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.baizhi.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author syf_12138
+ * @Description 测试控制类
+ * @create 2022/8/15 16:45
+ */
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        System.out.println("index ok!");
+        return "index ok";
+    }
+}
