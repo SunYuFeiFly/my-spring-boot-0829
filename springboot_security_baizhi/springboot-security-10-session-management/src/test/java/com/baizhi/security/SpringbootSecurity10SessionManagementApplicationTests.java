3795a9a0af597b7a99033c549111873f93c30cbf
gaozy124 <298012895@qq.com>
2022-08-15 16:48:40 +0800

    Spring Boot 整合security 会话管理

---
+++ springboot_security_baizhi/springboot-security-10-session-management/src/test/java/com/baizhi/security/SpringbootSecurity10SessionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity10SessionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
