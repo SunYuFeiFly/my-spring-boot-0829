9c586f7e40b4c91e0f2695784a47ff2e12a7acb7
gaozy124 <298012895@qq.com>
2022-08-15 15:06:31 +0800

    主启动类

--- springboot_security_baizhi/springboot-security-08-rememberme-web/src/main/java/com/baizhi/security/SpringbootSecurity08RemembermeWebApplication.java
+++ springboot_security_baizhi/springboot-security-08-rememberme-web/src/main/java/com/baizhi/security/SpringbootSecurity08RemembermeWebApplication.java
@@ -1,4 +1,4 @@
-package com.baizhi.security.springbootsecurity08remembermeweb;
+package com.baizhi.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
