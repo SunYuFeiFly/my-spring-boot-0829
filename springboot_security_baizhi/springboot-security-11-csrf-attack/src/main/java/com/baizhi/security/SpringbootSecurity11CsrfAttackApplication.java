c4168759c86e9994412b6fb3a3eed63c72ef4ea4
syf_12138 <1049924914@qq.com>
2022-08-15 21:41:18 +0800

    Spring Boot整合Security 防止crsf攻击

---
+++ springboot_security_baizhi/springboot-security-11-csrf-attack/src/main/java/com/baizhi/security/SpringbootSecurity11CsrfAttackApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity11CsrfAttackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity11CsrfAttackApplication.class, args);
+    }
+
+}
---
+++ springboot_security_baizhi/springboot-security-11-csrf-attack/src/test/java/com/baizhi/security/SpringbootSecurity11CsrfAttackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity11CsrfAttackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
