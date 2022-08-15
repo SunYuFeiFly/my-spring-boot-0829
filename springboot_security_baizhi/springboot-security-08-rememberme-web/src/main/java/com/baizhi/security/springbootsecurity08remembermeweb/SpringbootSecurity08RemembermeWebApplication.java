edd9448eeb1555fc67291f9e03286a2e9e5f7af7
gaozy124 <298012895@qq.com>
2022-08-15 14:10:42 +0800

    Spring Boot 整合Security web版实现记住我功能

---
+++ springboot_security_baizhi/springboot-security-08-rememberme-web/src/main/java/com/baizhi/security/springbootsecurity08remembermeweb/SpringbootSecurity08RemembermeWebApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.security.springbootsecurity08remembermeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity08RemembermeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity08RemembermeWebApplication.class, args);
+    }
+
+}
---
+++ springboot_security_baizhi/springboot-security-08-rememberme-web/src/test/java/com/baizhi/security/springbootsecurity08remembermeweb/SpringbootSecurity08RemembermeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.security.springbootsecurity08remembermeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity08RemembermeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
