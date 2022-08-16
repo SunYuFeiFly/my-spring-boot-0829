0c61daf9c881acb91511b159e3ddbe0b584b6af6
gaozy124 <298012895@qq.com>
2022-08-16 17:49:13 +0800

    Spring Boot 整合 Security 授权

---
+++ springboot_security_baizhi/springboot-security-15-authorize/src/main/java/com/baizhi/security/SpringbootSecurity15AuthorizeApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity15AuthorizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity15AuthorizeApplication.class, args);
+    }
+
+}
