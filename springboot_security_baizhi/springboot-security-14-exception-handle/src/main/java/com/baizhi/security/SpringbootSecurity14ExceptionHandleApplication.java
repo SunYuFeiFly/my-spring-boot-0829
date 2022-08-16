dbc601c18218a6f683aa048d022e90cb345c1f69
gaozy124 <298012895@qq.com>
2022-08-16 17:38:14 +0800

    主启动类

--- springboot_security_baizhi/springboot-security-14-exception-handle/src/main/java/com/baizhi/security/SpringbootSecurity14ExceptionHandleApplication.java
+++ springboot_security_baizhi/springboot-security-14-exception-handle/src/main/java/com/baizhi/security/SpringbootSecurity14ExceptionHandleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootSecurity15ExceptionHandleApplication {
+public class SpringbootSecurity14ExceptionHandleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootSecurity15ExceptionHandleApplication.class, args);
+        SpringApplication.run(SpringbootSecurity14ExceptionHandleApplication.class, args);
     }
 
 }
