3f0289dcf1fba23324f4f3b04e0f0b3cc081970c
gaozy124 <298012895@qq.com>
2022-08-16 17:15:36 +0800

    自定义security配置实现跨域

---
+++ springboot_security_baizhi/springboot-security-13-cors/src/main/java/com/baizhi/security/config/MySecurityConfigure.java
@@ -0,0 +1,87 @@
+package com.baizhi.security.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author syf_12138
+ * @Description 自定义资源权限规则
+ * @create 2022/8/16 16:57
+ */
+
+@Configuration
+public class MySecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 开启请求权限管理
+        http.authorizeRequests()
+            // 所有请求在认证之后才能访问
+            .anyRequest().authenticated()
+            // 开启表单认证
+            .and().formLogin()
+
+            // 开启跨域
+            .and().cors().configurationSource(configurationSource())
+
+            // 认证异常
+            .and().exceptionHandling()
+            // 异常处理
+            .authenticationEntryPoint((request, response, exception) -> {
+                response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.getWriter().println("请认证之后再去处理!");
+            })
+            // 开启注销登录，拿到注销登录处理对象
+            .and().logout()
+            // 指定注销登录的url(默认/logout，GET方式)
+            // .logoutUrl("/logout")
+            // 多个请求都能实现退出登录需求，且请求方式不固定为GET
+            .logoutRequestMatcher(new OrRequestMatcher(
+                    new AntPathRequestMatcher("/logout", HttpMethod.GET.name()),
+                    new AntPathRequestMatcher("/logout",HttpMethod.DELETE.name())))
+            // 对否让session失效
+            .invalidateHttpSession(true)
+            // 清除认证标记
+            .clearAuthentication(true)
+            // 注销成功跳转的请求(适用传统web开发)
+            // .logoutSuccessUrl("/login.html")
+            // 注销成功，返回自定义json内容（适合前后端分离系统）
+            .logoutSuccessHandler((request, response, authentication) -> {
+                Map<String, Object> result = new HashMap<String, Object>();
+                result.put("msg", "注销成功");
+                result.put("用户信息", authentication.getPrincipal());
+                response.setContentType("application/json;charset=UTF-8");
+                response.setStatus(HttpStatus.OK.value());
+                String s = new ObjectMapper().writeValueAsString(result);
+                response.getWriter().println(s);
+            })
+            // 禁止csrf跨站请求保护
+            .and().csrf().disable();
+    }
+
+    CorsConfigurationSource configurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+}
