fdc83d508a5988de4e5ad87936cc1065ca667d9a
gaozy124 <298012895@qq.com>
2022-08-16 17:51:13 +0800

    Spring Boot 整合 Security 动态授权

---
+++ springboot_security_baizhi/springboot-security-16-dynamic-authorize/src/main/java/com/baizhi/security/SpringbootSecurity16DynamicAuthorizeApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity16DynamicAuthorizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity16DynamicAuthorizeApplication.class, args);
+    }
+
+}
