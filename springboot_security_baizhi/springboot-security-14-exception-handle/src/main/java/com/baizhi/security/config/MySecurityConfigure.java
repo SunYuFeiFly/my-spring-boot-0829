028e08eb072bf3deca55dc05c2846c62fb728cc7
gaozy124 <298012895@qq.com>
2022-08-16 17:34:32 +0800

    自定义资源权限规则

--- springboot_security_baizhi/springboot-security-14-exception-handle/src/main/java/com/baizhi/security/config/MySecurityConfigure.java
+++ springboot_security_baizhi/springboot-security-14-exception-handle/src/main/java/com/baizhi/security/config/MySecurityConfigure.java
@@ -1,12 +1,17 @@
 package com.baizhi.security.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 
@@ -21,6 +26,21 @@ import java.util.Map;
 
 @Configuration
 public class MySecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService UserDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername("syf").password("{noop}123456").roles("admin").build());
+        return inMemoryUserDetailsManager;
+    }
+
+    /**
+     * 自定义认证规则（推荐，当系统默认和自定义都存在时，默认自定义会替代默认,是工厂中的实现，不可以在其它地方注入使用）
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+         auth.userDetailsService(UserDetailsService());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
