40f40d6e93c0e094923f9c4e8a342dac0fd34623
gaozy124 <298012895@qq.com>
2022-08-16 09:59:58 +0800

    spring boot 整合 Security 防止csrf攻击

---
+++ springboot_security_baizhi/springboot-security-11-csrf-blank/src/main/java/com/baizhi/security/SpringbootSecurity11CsrfBlankApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity11CsrfBlankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity11CsrfBlankApplication.class, args);
+    }
+
+}
