09525069b28a83765a955408df1f49578e543939
gaozy124 <298012895@qq.com>
2022-08-18 13:11:17 +0800

    消息提供者及工具类升级

--- springboo2_rabbitmq_baizhi/spring-rabbitmq-01-helloword/src/main/java/helloword/provider/Provider.java
+++ springboo2_rabbitmq_baizhi/spring-rabbitmq-01-helloword/src/main/java/helloword/provider/Provider.java
@@ -3,6 +3,7 @@ package helloword.provider;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import helloword.utils.RabbitMQUtil;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -57,6 +58,37 @@ public class Provider {
             // 关闭资源
             channel.close();
             connection.close();
+        }
+    }
+
+
+    @Test
+    public void testSendMessageUpgrade() throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            connection = RabbitMQUtil.getConnection();
+            // 获取通信连接通道
+            channel = connection.createChannel();
+            // 通道绑定对应消息队列
+            // 参数1: 队列名称(不存在的时候自动创建)
+            // 参数2: 用来定义队列特征是要持久化
+            // 参数3: 是否独占队列(true 就只能被当前连接使用)
+            // 参数4: 是否在消费完成后自动删除队列
+            // 参数5: 附加参数
+            channel.queueDeclare("hello", false, false, false, null);
+            // 发布消息
+            // 参数1: 交换机名称（此处通道直接连接队列，不用交换机）
+            // 参数2: 队列名称
+            // 参数3: 传递参数的额外设置
+            // 参数4: 消息的具体内容（需转成byte字节）
+            channel.basicPublish("", "hello", null, "hello rbbitmq_12138".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("消息已发布");
+            // 关闭资源
+            RabbitMQUtil.closeConnection(channel, connection);
         }
     }
 }
