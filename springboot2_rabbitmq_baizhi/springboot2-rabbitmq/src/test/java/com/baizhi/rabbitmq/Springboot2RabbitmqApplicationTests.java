3c3356501a20cb0060ba79aa3464e3d8e13acdfb
gaozy124 <298012895@qq.com>
2022-08-19 13:58:42 +0800

    Spring Boot2整合 RabbitMQ

---
+++ springboot2_rabbitmq_baizhi/springboot2-rabbitmq/src/test/java/com/baizhi/rabbitmq/Springboot2RabbitmqApplicationTests.java
@@ -0,0 +1,14 @@
+package com.baizhi.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Springboot2RabbitmqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
