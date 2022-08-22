a41eef5567e2273c67fe1573a5f3aaf8957ab7a2
gaozy124 <298012895@qq.com>
2022-08-22 17:35:55 +0800

    支付控制类

--- springcloud2_atguigu/springcloud-01-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ springcloud2_atguigu/springcloud-01-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,9 +4,13 @@ import com.atguigu.springcloud.entity.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import com.atguigu.springcloud.utils.CommonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author syf_12138
@@ -18,17 +22,23 @@ import javax.annotation.Resource;
 @RestController
 public class PaymentController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
-        log.info("*****插入操作返回结果:" + result);
+        log.info("*****新增结果:" + result);
         if (result > 0) {
-            return new CommonResult(200, "插入数据库成功", result);
+            return new CommonResult(200, "插入成功,返回结果" + result + "\t 服务端口：" + serverPort, payment);
         } else {
-            return new CommonResult(444, "插入数据库失败", null);
+            return new CommonResult(444, "插入失败", null);
         }
     }
 
@@ -37,9 +47,23 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****查询结果:{}", payment);
         if (payment != null) {
-            return new CommonResult(200, "查询成功", payment);
+            return new CommonResult(200, "查询成功" + "\t 服务端口：" + serverPort, payment);
         } else {
             return new CommonResult(444, "没有对应记录,查询ID: " + id, null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.discoveryClient;
     }
 }
