b9ac15c07024716e9ffe3024f57c0eddf615628d
gaozy124 <298012895@qq.com>
2022-08-22 17:31:59 +0800

    公用实体类

--- springcloud2_atguigu/springcloud-00-api-commons/src/main/java/com/atguigu/springcloud/entity/Payment.java
+++ springcloud2_atguigu/springcloud-00-api-commons/src/main/java/com/atguigu/springcloud/entity/Payment.java
@@ -8,15 +8,15 @@ import java.io.Serializable;
 
 /**
  * @author syf_12138
- * @Description 支付实体类
- * @create 2022/8/22 11:57
+ * @Description 公用实体类
+ * @create 2022/8/22 15:01
  */
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class Payment implements Serializable {
-
+    
     private Long id;
     private String serial;
 }
