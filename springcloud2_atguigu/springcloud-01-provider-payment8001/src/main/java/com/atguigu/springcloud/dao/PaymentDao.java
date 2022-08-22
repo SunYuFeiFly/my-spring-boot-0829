c9589f1241dcedc8dc14dc301cf2d30cbf6242fd
gaozy124 <298012895@qq.com>
2022-08-22 13:41:08 +0800

    支付映射接口

---
+++ springcloud2_atguigu/springcloud-01-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.dao;
+
+import com.atguigu.springcloud.entity.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author syf_12138
+ * @Description 支付映射接口
+ * @create 2022/8/22 12:09
+ */
+
+@Mapper
+public interface PaymentDao {
+
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+}
