72adb75da2cb22eb10f12ff80ab4330cb72d2516
gaozy124 <298012895@qq.com>
2022-08-22 17:39:24 +0800

    支付服务接口实现类

---
+++ springcloud2_atguigu/springcloud-01-provider-payment8002/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.PaymentDao;
+import com.atguigu.springcloud.entity.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author syf_12138
+ * @Description 支付服务接口实现类
+ * @create 2022/8/22 16:01
+ */
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+
+}
