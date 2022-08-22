87bcd5baa1af648ae7d198d718e1092e5e0091a6
gaozy124 <298012895@qq.com>
2022-08-22 17:44:08 +0800

    Eureka注册中心主启动类

---
+++ springcloud2_atguigu/springcloud-03-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author syf_12138
+ * @Description Eureka注册中心主启动类
+ * @create 2022/8/22 15:40
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
