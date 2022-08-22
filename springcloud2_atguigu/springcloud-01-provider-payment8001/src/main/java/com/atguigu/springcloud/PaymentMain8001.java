98120a10ff068e2f4ef0d6db73afa98ca2a83a62
gaozy124 <298012895@qq.com>
2022-08-22 13:42:25 +0800

    生产者主启动类

---
+++ springcloud2_atguigu/springcloud-01-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author syf_12138
+ * @Description 生产者主启动类
+ * @create 2022/8/22 11:49
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
+
