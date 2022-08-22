3bed57627a6129c51cd93ec31f5e639e7cf9c7be
gaozy124 <298012895@qq.com>
2022-08-22 17:39:37 +0800

    生产者主启动类

---
+++ springcloud2_atguigu/springcloud-01-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author syf_12138
+ * @Description 生产者主启动类
+ * @create 2022/8/22 15:59
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class PaymentMain8002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class, args);
+    }
+}
