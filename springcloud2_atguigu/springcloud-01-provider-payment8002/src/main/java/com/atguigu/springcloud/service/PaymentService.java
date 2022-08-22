b3909eeefa5b04b417b6f9a163d72466403c8c44
gaozy124 <298012895@qq.com>
2022-08-22 17:39:14 +0800

    支付服务接口

---
+++ springcloud2_atguigu/springcloud-01-provider-payment8002/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud.service;
+
+
+import com.atguigu.springcloud.entity.Payment;
+
+/**
+ * @author syf_12138
+ * @Description 支付服务接口
+ * @create 2022/8/22 16:01
+ */
+
+public interface PaymentService {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(Long id);
+}
