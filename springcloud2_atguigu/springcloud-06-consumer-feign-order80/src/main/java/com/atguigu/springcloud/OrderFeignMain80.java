125f05d1f2845b9bcd38985c1a6cc7928eea3273
gaozy124 <298012895@qq.com>
2022-08-23 17:54:57 +0800

    Feign消费者主启动类

---
+++ springcloud2_atguigu/springcloud-06-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author syf_12138
+ * @Description Feign消费者主启动类
+ * @create 2022/8/23 15:29
+ */
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
