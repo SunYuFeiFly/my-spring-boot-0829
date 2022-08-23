34032b0c7214bd347192851bae5c98ce96164ce9
gaozy124 <298012895@qq.com>
2022-08-23 10:44:41 +0800

    消费控制类

---
+++ springcloud2_atguigu/springcloud-04-consumerzk-order80/src/main/java/com/atguigu/springcloud/controller/OrderZKController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author syf_12138
+ * @Description 消费控制类
+ * @create 2022/8/23 10:38
+ */
+
+@RestController
+public class OrderZKController {
+
+    public static final String INVOKE_URL = "http://springcloud-provider-payment";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/consumer/payment/zk")
+    public String paymentInfo() {
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        System.out.println("消费者调用支付服务(zookeeper)--->result:" + result);
+        return result;
+    }
+}
