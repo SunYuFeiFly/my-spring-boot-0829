6c6f7943a172a7cf1b44815a963371be9ceae90b
gaozy124 <298012895@qq.com>
2022-08-23 11:20:06 +0800

    生产端控制类

---
+++ springcloud2_atguigu/springcloud-05-providerconsul-payment8006/src/main/java/com/stguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.stguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author syf_12138
+ * @Description TODO
+ * @create 2022/8/23 10:59
+ */
+
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/consul")
+    public String paymentInfo() {
+        return "springcloud with consul: " + serverPort + "\t\t" + UUID.randomUUID().toString();
+    }
+}
