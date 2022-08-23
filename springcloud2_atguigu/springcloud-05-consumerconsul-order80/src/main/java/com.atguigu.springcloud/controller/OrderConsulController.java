2034b86a49a8a192f7a9f10d47315ada85ca42fa
gaozy124 <298012895@qq.com>
2022-08-23 11:18:42 +0800

    消费控制类

---
+++ springcloud2_atguigu/springcloud-05-consumerconsul-order80/src/main/java/com.atguigu.springcloud/controller/OrderConsulController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author syf_12138
+ * @Description 消费控制类
+ * @create 2022/8/23 11:11
+ */
+
+@RestController
+public class OrderConsulController {
+
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/consul")
+    public String paymentInfo() {
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/consul", String.class);
+        System.out.println("消费者调用支付服务(consule)--->result:" + result);
+        return result;
+    }
+}
+
