c2e552d2b67d50f8fb7521e6d49d162a2a180e0d
gaozy124 <298012895@qq.com>
2022-08-23 11:17:59 +0800

    消费端主启动类

---
+++ springcloud2_atguigu/springcloud-05-consumerconsul-order80/src/main/java/com.atguigu.springcloud/OrderConsulMain80.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author syf_12138
+ * @Description 消费端主启动类
+ * @create 2022/8/23 11:09
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+}
