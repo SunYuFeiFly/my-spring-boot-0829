56d0f995f2887b8e11d9d9d16fa67d064d29cbc3
gaozy124 <298012895@qq.com>
2022-08-23 18:05:52 +0800

    消费者控制类

--- springcloud2_atguigu/springcloud-02-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ springcloud2_atguigu/springcloud-02-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,13 +1,21 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entity.Payment;
+import com.atguigu.springcloud.loadBalancer.LoadBalancer;
 import com.atguigu.springcloud.utils.CommonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @author syf_12138
@@ -25,6 +33,12 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/payment/create") //客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
     public CommonResult create(Payment payment) {
         return restTemplate.postForObject(PaymentSrv_URL + "/payment/create", payment, CommonResult.class);
@@ -34,5 +48,31 @@ public class OrderController {
     @GetMapping("/payment/get/{id}")
     public CommonResult getPayment(@PathVariable Long id) {
         return restTemplate.getForObject(PaymentSrv_URL + "/payment/get/" + id, CommonResult.class, id);
+    }
+
+
+    @GetMapping("/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PaymentSrv_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
+    }
+
+
+    /**
+     * 测试自己编写负载均衡
+     */
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD-PAYMENT-SERVICE");
+        if (instances == null || instances.size() <= 0) {
+            return null;
+        }
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(uri + "/payment/lb/", String.class);
     }
 }
