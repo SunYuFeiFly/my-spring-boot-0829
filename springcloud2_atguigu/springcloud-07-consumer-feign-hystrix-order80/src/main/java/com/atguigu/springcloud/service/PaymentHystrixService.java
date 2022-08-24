e340e35699658694b0eb0b73d70f7c5955887826
gaozy124 <298012895@qq.com>
2022-08-24 17:55:39 +0800

    远程服务调用接口

--- springcloud2_atguigu/springcloud-07-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
+++ springcloud2_atguigu/springcloud-07-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
  */
 
 @Component
-@FeignClient(value = "SPRINGCLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value = "SPRINGCLOUD-PROVIDER-HYSTRIX-PAYMENT", fallback = PaymentFallbackService.class)
 public interface PaymentHystrixService {
 
-    @GetMapping("/payment/hystrix/ok/{id}")
-    public String paymentInfo_OK(@PathVariable("id") Integer id);
+    @GetMapping("/hystrixPayment/hystrix/ok/{id}")
+    public String paymentInfoOK(@PathVariable("id") Integer id);
 
-    @GetMapping("/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+    @GetMapping("/hystrixPayment/hystrix/timeout/{id}")
+    public String paymentInfoTimeOut(@PathVariable("id") Integer id);
 }
