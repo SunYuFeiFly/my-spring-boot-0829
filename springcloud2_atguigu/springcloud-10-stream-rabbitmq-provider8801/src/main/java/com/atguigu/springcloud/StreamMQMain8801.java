dd6846f6daea3c39c4818dbf27c50518aaceb3d8
gaozy124 <298012895@qq.com>
2022-08-25 16:26:38 +0800

    消息发送主启动类

---
+++ springcloud2_atguigu/springcloud-10-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/StreamMQMain8801.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author syf_12138
+ * @Description 消息发送主启动类
+ * @create 2022/8/25 14:58
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8801 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8801.class, args);
+    }
+}
