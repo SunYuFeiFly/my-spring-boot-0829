c63f931c7e07a156d58a50d36813adc527b9d465
gaozy124 <298012895@qq.com>
2022-08-25 16:27:05 +0800

    消息推送接口

---
+++ springcloud2_atguigu/springcloud-10-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud.service;
+
+/**
+ * @author syf_12138
+ * @Description 消息推送接口
+ * @create 2022/8/25 14:58
+ */
+
+public interface IMessageProvider {
+
+    public String send();
+}
