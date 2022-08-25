a408543d019e89b3f04338377a2467f340937e60
gaozy124 <298012895@qq.com>
2022-08-25 16:24:38 +0800

    自定义路由

---
+++ springcloud2_atguigu/springcloud-09-gateway-gateway9527/src/main/java/com.atguigu.springcloud/config/MyGateWayConfig.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author syf_12138
+ * @Description 自定义路由
+ * @create 2022/8/25 10:54
+ */
+
+@Configuration
+public class MyGateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_atguigu",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
