815e561fe8abdee26b770ffb24af88094ba5b165
gaozy124 <298012895@qq.com>
2022-08-25 18:14:12 +0800

    服务消费端主启动类

---
+++ springcloud2_atguigu/springcloudalibaba-01-consumer-nacos-order83/src/main/java/com/atguigu/alibaba/OrderNacosMain83.java
@@ -0,0 +1,20 @@
+package com.atguigu.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author syf_12138
+ * @Description 服务消费端主启动类
+ * @create 2022/8/25 18:02
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
