9beec54a9b4b193a8b35f37d6c84daba2b65ef51
gaozy124 <298012895@qq.com>
2022-08-25 18:09:22 +0800

    服务提供端主启动类

---
+++ springcloud2_atguigu/springcloudalibaba-01-provider-payment9001/src/main/java/com/atguigu/alibaba/PaymentMain9001.java
@@ -0,0 +1,21 @@
+package com.atguigu.alibaba;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author syf_12138
+ * @Description 服务提供端主启动类
+ * @create 2022/8/25 17:49
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
