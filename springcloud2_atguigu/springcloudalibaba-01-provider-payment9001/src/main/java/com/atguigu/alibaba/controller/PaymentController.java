6be8959512b3bf0d58299bac946ded632a6309f6
gaozy124 <298012895@qq.com>
2022-08-25 18:10:06 +0800

    服务提供控制类

---
+++ springcloud2_atguigu/springcloudalibaba-01-provider-payment9001/src/main/java/com/atguigu/alibaba/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.atguigu.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author syf_12138
+ * @Description 服务提供控制类
+ * @create 2022/8/25 17:51
+ */
+
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id " + id;
+    }
+}
