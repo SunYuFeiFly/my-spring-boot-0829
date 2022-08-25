f2aba74b4eeaac28415c1684e3ca6b135f579c22
gaozy124 <298012895@qq.com>
2022-08-25 16:23:47 +0800

    主启动类

---
+++ springcloud2_atguigu/springcloud-09-gateway-gateway9527/src/main/java/com.atguigu.springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author syf_12138
+ * @Description TODO
+ * @create 2022/8/25 10:05
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
