66d33e89757e32f39bc3705f9aa94f7e8a5baf49
gaozy124 <298012895@qq.com>
2022-08-25 16:27:46 +0800

    消息推送控制类

---
+++ springcloud2_atguigu/springcloud-10-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/SendMessageController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author syf_12138
+ * @Description 消息推送控制类
+ * @create 2022/8/25 15:12
+ */
+
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+}
