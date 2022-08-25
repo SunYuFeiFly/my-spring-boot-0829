1623de81bf0c33ca6558fb353e52504cff5e7a22
gaozy124 <298012895@qq.com>
2022-08-25 16:29:44 +0800

    监听消息接收类

---
+++ springcloud2_atguigu/springcloud-10-stream-rabbitmq-consumer8803/src/main/java/com.atguigu.springcloud/service/ReceiveMessageListener.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author syf_12138
+ * @Description 监听消息接收类
+ * @create 2022/8/25 15:26
+ */
+
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListener {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者2号，------->接收到的消息：" + message.getPayload() + "\t port: " + serverPort);
+    }
+}
