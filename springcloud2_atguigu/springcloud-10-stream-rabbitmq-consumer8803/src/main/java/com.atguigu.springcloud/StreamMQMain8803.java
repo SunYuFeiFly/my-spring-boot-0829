2cf41447436a99eb0251e0f62ebf98592cf9a445
gaozy124 <298012895@qq.com>
2022-08-25 16:29:23 +0800

    接收消息主启动类

---
+++ springcloud2_atguigu/springcloud-10-stream-rabbitmq-consumer8803/src/main/java/com.atguigu.springcloud/StreamMQMain8803.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author syf_12138
+ * @Description 接收消息主启动类
+ * @create 2022/8/25 15:23
+ */
+
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
