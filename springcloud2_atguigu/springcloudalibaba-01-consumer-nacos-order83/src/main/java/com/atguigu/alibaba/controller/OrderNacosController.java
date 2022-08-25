6a89957e10b05bd62540f50366da41fd5e33a49f
gaozy124 <298012895@qq.com>
2022-08-25 18:14:44 +0800

    消费端控制类

---
+++ springcloud2_atguigu/springcloudalibaba-01-consumer-nacos-order83/src/main/java/com/atguigu/alibaba/controller/OrderNacosController.java
@@ -0,0 +1,31 @@
+package com.atguigu.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author syf_12138
+ * @Description 消费端控制类
+ * @create 2022/8/25 18:04
+ */
+
+@RestController
+public class OrderNacosController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping("/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+
+}
